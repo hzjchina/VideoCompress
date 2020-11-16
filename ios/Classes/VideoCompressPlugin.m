78d31fb22c34eb1b7e031e17d5cd64d9eabd22da
Jet(子敬) <test@test.com>
2020-11-16 17:03:52 +0800

    引入修改

--- ios/Classes/VideoCompressPlugin.m
+++ ios/Classes/VideoCompressPlugin.m
@@ -1,5 +1,13 @@
 #import "VideoCompressPlugin.h"
+#if __has_include(<video_compress/video_compress-Swift.h>)
 #import <video_compress/video_compress-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "video_compress-Swift.h"
+#endif
+
 
 @implementation VideoCompressPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
